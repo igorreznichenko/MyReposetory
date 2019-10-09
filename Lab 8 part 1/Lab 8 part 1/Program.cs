39d84af4abf96332b0c9d12809b4723955023fb7
Igor <igorteamhtc@live.ru>
2019-10-09 20:31:56 +0300

    lab8

---
+++ Lab 8 part 1/Lab 8 part 1/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8_part_1
+{
+    class Auto
+    {
+        public string name;
+        public string color;
+        public int year;
+        public Auto(string Name, string Color, int Year)
+        {
+            name = Name;
+            year = Year;
+            color = Color;
+        }
+    }
+    class Garage_Of_Sheikh
+    {
+        public List<Auto> auto = new List<Auto>();
+
+        public void ShowCars()
+        {
+            if(auto.Count == 0)
+            {
+                Console.WriteLine("Error : Garage is empty !");
+                return;
+            }
+            foreach(Auto l in auto){
+                Console.WriteLine("Auto : " + l.name);
+                Console.WriteLine("Color : " + l.color);
+                Console.WriteLine("Year : " + l.year);
+                Console.WriteLine();
+            }
+        }
+        public void ChooseCar()
+        {
+            if (auto.Count == 0)
+            {
+                Console.WriteLine("Error : Garage is empty !");
+                return;
+            }
+            bool b;
+            string name;
+            string color;
+            int year;
+            Console.WriteLine("Input parameters, if parametr isn't mandatory input 0");
+            Console.WriteLine("Input name");
+            name = Console.ReadLine();
+            Console.WriteLine("Color : ");
+            color = Console.ReadLine();
+            Console.WriteLine("Year : ");
+            
+            do
+            {
+                b = int.TryParse(Console.ReadLine(), out year);
+                if (b == false)
+                    Console.WriteLine("Error : You try to input string value !");
+                if (year < 0)
+                    Console.WriteLine("Input positive number !");
+
+            } while (year < 0 || b == false);
+             foreach(Auto car in auto)
+            {
+                if(car.name == name || name == "0")
+                    if (car.color == color || color=="0")
+                        if (car.year == year || year == 0)
+                        {
+                            Console.WriteLine("Auto : " + car.name);
+                            Console.WriteLine("Color : " + car.color);
+                            Console.WriteLine("Year : " + car.year);
+                            Console.WriteLine();
+                        }
+            }
+
+          
+        }
+        public bool DeleteCar()
+        {
+            if (auto.Count == 0)
+            {
+                Console.WriteLine("Error : Garage is empty !");
+                return false;
+            }
+            bool b;
+            string name;
+            string color;
+            int year;
+            Console.WriteLine("Input parameters, if parametr isn't mandatory input 0");
+            Console.WriteLine("Input name");
+            name = Console.ReadLine();
+            Console.WriteLine("Color : ");
+            color = Console.ReadLine();
+            Console.WriteLine("Year : ");
+
+            do
+            {
+                b = int.TryParse(Console.ReadLine(), out year);
+                if (b == false)
+                    Console.WriteLine("Error : You try to input string value !");
+                if (year < 1)
+                    Console.WriteLine("Input positive number !");
+
+            } while (year < 1 || b == false);
+
+            b = auto.Remove(new Auto(name, color, year));
+            return b;
+        }
+
+        public void BuyNewCar() {
+          
+            bool b;
+            string name;
+            string color;
+            int year;
+            Console.WriteLine("Input name");
+            name = Console.ReadLine();
+            Console.WriteLine("Color : ");
+            color = Console.ReadLine();
+            Console.WriteLine("Year : ");
+
+            do
+            {
+                b = int.TryParse(Console.ReadLine(), out year);
+                if (b == false)
+                    Console.WriteLine("Error : You try to input string value !");
+                if (year < 1)
+                    Console.WriteLine("Input positive number !");
+
+            } while (year < 1 || b == false);
+
+            auto.Add(new Auto(name, color, year));
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Garage_Of_Sheikh Garage = new Garage_Of_Sheikh();
+            int i;
+            bool b;
+            do
+            {
+                Console.WriteLine("1 : See the cars");
+                Console.WriteLine("2 : Choose car");
+                Console.WriteLine("3 : Delete car");
+                Console.WriteLine("4 : Buy new car");
+                Console.WriteLine("5 : Exit");
+                do
+                {
+                    Console.Write("Your choice is : ");
+                    b = int.TryParse(Console.ReadLine(), out i);
+                    if (b == false)
+                        Console.WriteLine("Error : You try to input string value !");
+                    else
+                    {
+                        if (i < 1 || i > 4)
+                            Console.WriteLine("Input i>=1 and i <=4");
+                    }
+
+                } while (b == false || i < 1 || i > 4);
+
+                switch (i) {
+                    case 1:
+                        {
+                            Garage.ShowCars();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Garage.ChooseCar();
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (Garage.DeleteCar())
+                                Console.WriteLine("Delete  complete");
+                            else
+                                Console.WriteLine("Delete didn't Complete");
+                            break;
+                        }
+                    case 4:
+                        {
+                            Garage.BuyNewCar();
+                            break;
+                        }
+                }
+               
+               
+            } while (i != 5);
+           
+        }
+    }
+}
---
+++ Lab 8 part 2/Lab 8 part 2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8_part_2
+{
+    class DiskPhone
+    {
+        public int price;
+        public int model;
+        public int weight;
+        public int mumber;
+        public void DoCall() { }
+        public void inputNumber() { }
+
+    }
+    class PhoneWithKeywords : DiskPhone {
+
+        public int AmountOfMemory;
+        public int NumberOfKeys;
+        
+    }
+    class PhoneWithBlackWhiteScreen : PhoneWithKeywords {
+        public int WidthOfScreen;
+        public int HeightOfScreen;
+        public int SignalStrength;
+        public string NameOfSimCard;
+        public void PlayInGame() { }
+        public void TurnOnCalculator() { }
+        public void UseCalendar() { }
+        public void UseAlarm() { }
+        public void UseTorch() { }
+        public void SentMessage() { }
+    }
+
+    class PhoneColorScreen : PhoneWithBlackWhiteScreen
+    {
+        public int AmountCameraPixels;
+
+        public void DoPhoto() { }
+        public void UseInternet() { }
+        public void PlayMusic() { }
+        public void UseVideocamera() { }
+        public void SeeVideos() { }
+        public void UseAditionalMemoryCard() { }
+        public void InstallApp() { }
+        public void SeeWeathwer() { }
+
+    }
+
+    class IPhone : PhoneColorScreen {
+
+        public void UseVideoCalls() { }
+        public void UseAppstore() { }
+       private void UseTouchId() { }
+    }
+    class GoogleGlass : IPhone {
+        
+
+        void ConnectToPhone() { }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GoogleGlass Glass = new GoogleGlass();
+        }
+    }
+}
