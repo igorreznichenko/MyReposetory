16aee795a1a808e5691755543cd39152817ef5cf
Igor <igorteamhtc@live.ru>
2019-11-18 19:02:05 +0200

    Lab11

---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    static class Hospital {
+       static public void MRT(ref Patient p) {
+            p.mrt_pass = true;
+        }
+       static  public void KT(ref Patient p) {
+            p.kt_pass = true;
+        }
+       static public void Rentgen(ref Patient p) {
+            p.rentgen_pass = true;
+        }
+       static public void LOR(ref Patient p) {
+            p.lor_pass = true;
+        }
+       static public void USI(ref Patient p) {
+            p.usi_pass = true;
+        }
+       static public void Remwo(ref Patient p) {
+            p.remwo_pass = true;
+        }
+        
+    
+    }
+    class Patient
+    {
+        public bool mrt_pass;
+        public bool kt_pass;
+        public bool rentgen_pass;
+        public bool lor_pass;
+        public bool usi_pass;
+        public bool remwo_pass;
+        public bool MRT_Pass {
+            get { return mrt_pass; }
+        }
+        public bool KT_Pass {
+            get { return kt_pass; }
+        }
+        public bool Rentgen_Pass
+        {
+            get { return rentgen_pass; }
+        }
+        public bool LOR_Pass
+        {
+            get { return lor_pass; }
+        }
+        public bool USI_Pass
+        {
+            get { return usi_pass; }
+        }
+        public bool Remwo_Pass
+        {
+            get { return remwo_pass; }
+        }
+        public void Get_Info()
+        {
+
+            Console.WriteLine("Mrt passed : {0}", MRT_Pass);
+            Console.WriteLine("KT passed : {0}", KT_Pass);
+            Console.WriteLine("Rentgen passed :{0}", Rentgen_Pass);
+            Console.WriteLine("LOR passed : {0}", LOR_Pass);
+            Console.WriteLine("USI passed : {0}", USI_Pass);
+            Console.WriteLine("Remwo passed : {0}", Remwo_Pass);
+        }
+    }
+    class Program
+    {
+        public delegate void Hospital_Work(ref Patient p);
+        static void Main(string[] args)
+        {       
+            Patient patient = new Patient();
+            Hospital_Work hospital = Hospital.MRT;
+            hospital += Hospital.KT;
+            hospital += Hospital.Rentgen;
+            hospital += Hospital.LOR;
+            hospital += Hospital.USI;
+            hospital += Hospital.Remwo;
+
+            Console.WriteLine("1: to pass MRT");
+            Console.WriteLine("2: to pass KT");
+            Console.WriteLine("3: to pass Rentgen");
+            Console.WriteLine("4: to pass LOR");
+            Console.WriteLine("5: to pass USI");
+            Console.WriteLine("6: to pass Remwo");
+            Console.WriteLine("7: to pass all");
+            Console.WriteLine("8: se status");
+            Console.WriteLine("0: Exit");
+
+            string s;
+            do
+            {
+              
+                s = Console.ReadLine();
+                switch(s){
+                    case "1":
+                        {
+                            Hospital.MRT(ref patient);
+                            Console.WriteLine("Operation complete");
+                            break;
+
+                        }
+                    case "2":
+                        {
+                            Hospital.KT(ref patient);
+                            Console.WriteLine("Operation complete");
+
+                            break;
+                        }
+                    case "3":
+                        {
+                            Hospital.Rentgen(ref patient);
+                            Console.WriteLine("Operation complete");
+
+                            break;
+                        }
+                    case "4":
+                        {
+                            Hospital.LOR(ref patient);
+                            Console.WriteLine("Operation complete");
+
+                            break;
+                        }
+                    case "5":
+                        {
+                            Hospital.USI(ref patient);
+                            Console.WriteLine("Operation complete");
+
+                            break;
+                        }
+                    case "6":
+                        {
+                            Hospital.Remwo(ref patient);
+                            Console.WriteLine("Operation complete");
+
+                            break;
+                        }
+                    case "7":
+                        {
+                            hospital(ref patient);
+                            Console.WriteLine("Operation complete");
+
+                            break;
+                        }
+                    case "8":
+                        {
+                            patient.Get_Info();
+                            break;
+                        }
+
+                }
+
+            } while (s != "0");
+        }
+    }
+}
---
+++ lab 11 part 2/lab 11 part 2/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_11_part_2
+{
+    static class Extension
+    {
+        static int i = 1;
+        public static List<Student> FindStudent(this List<Student> students, StudentPredicateDelegate Check_Student)
+        {
+            List<Student> result = new List<Student>();
+            foreach (Student l in students)
+            {
+                if (Check_Student(l))
+                    result.Add(l);
+            }
+            return result;
+        }
+        public static void Show_List(this List<Student> Student_List)
+        {
+            Console.WriteLine("Result {0}", i);
+            foreach (Student l in Student_List)
+            {               
+              Console.WriteLine("firstName : {0}", l.FirstName);
+                Console.WriteLine("lastName : {0}", l.LastName);
+                Console.WriteLine("Age : {0}", l.Age);
+            }
+            Console.WriteLine();
+            i++;
+
+        }
+    }
+   public class Student
+    {
+        string firstname;
+        string lastname;
+        int age;
+        public Student(string firstname,string lastname, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+        }
+        public string FirstName
+        {
+            get {return firstname;}
+           
+        }
+        public string LastName
+        {
+            get { return lastname;}
+            
+        }
+        public int Age
+        {
+            get { return age;}
+          
+        }
+        static public bool Check_Age(Student s)
+        {
+
+            return s.Age >= 18;
+        }
+        static public bool Check_First_Name(Student s)
+        {
+            return s.FirstName[0] == 'A';
+        }
+        static public bool Check_Last_Name(Student s)
+        {
+            return s.LastName.Length > 3;
+        }
+    }
+    public delegate bool StudentPredicateDelegate(Student s);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<Student> Student_List = new List<Student>();
+            List<Student> Result_List = new List<Student>();
+            Student_List.Add(new Student("Vania", "Petrenko", 17));
+            Student_List.Add(new Student("Kiril", "Danilenko", 19));
+            Student_List.Add(new Student("Vadim", "Petruk", 18));
+            Student_List.Add(new Student("Anna", "Klimenuk", 18));
+            Student_List.Add(new Student("Andrew", "Petrath", 16));
+            Student_List.Add(new Student("Ilia", "Makosiy", 17));
+            Student_List.Add(new Student("Vasia", "Petrovich", 16));
+            Student_List.Add(new Student("Anton", "Berezeckiy", 20));
+            Student_List.Add(new Student("Yaroslav", "Troeslen", 25));
+            Student_List.Add(new Student("Yaroslav", "Tarasenko", 25));
+            Student_List.Add(new Student("Petro", "Bojko", 25));
+
+            StudentPredicateDelegate check = Student.Check_Last_Name;
+            Result_List = Student_List.FindStudent(check);
+            Result_List.Show_List();
+
+            check = Student.Check_First_Name;
+            Result_List = Student_List.FindStudent(check);
+            Result_List.Show_List();
+
+            check = Student.Check_Age;
+            Result_List = Student_List.FindStudent(check);
+            Result_List.Show_List();
+
+            check = (s)=>s.Age >= 18;
+            Result_List = Student_List.FindStudent(check);
+            Result_List.Show_List();
+
+            check = (s)=>s.FirstName[0] == 'A';
+            Result_List = Student_List.FindStudent(check);
+            Result_List.Show_List();
+
+            check = (s)=>s.LastName.Length > 3;
+            Result_List = Student_List.FindStudent(check);
+            Result_List.Show_List();
+
+
+            check = (s) => s.Age > 20 && s.Age <= 25;
+            Result_List = Student_List.FindStudent(check);
+            Result_List.Show_List();
+
+
+            check = (s) => s.FirstName == "Andrew";
+            Result_List = Student_List.FindStudent(check);
+            Result_List.Show_List();
+
+
+            check = (s) => s.LastName == "Troeslen";
+            Result_List = Student_List.FindStudent(check);
+            Result_List.Show_List();
+
+        }
+    }
+}
