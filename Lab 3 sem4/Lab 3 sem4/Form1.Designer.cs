7fb936c54143656f231fbb56cd5142f1a18808db
Igor <igorteamhtc@live.ru>
2020-03-06 12:04:47 +0200

    Lab3_Sem2

---
+++ Lab 3 sem4/Lab 3 sem4/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Lab_3_sem4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(30, 21);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(534, 386);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(526, 360);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(32, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.progressBar1);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(526, 360);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(399, 327);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(183, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Play again";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(52, 286);
+            this.progressBar1.Maximum = 16;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(420, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 324);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab 3 sem4/Lab 3 sem4/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3_sem4
+{
+    public partial class Form1 : Form
+    {
+        int n;
+        Button[,] ButtonsArr;
+        int counter = 1;
+        int seconds = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        public void MixNumbers()
+        {
+            int c = counter;
+            int i, j, k;
+
+            List<int> arr = new List<int>();
+            Random r = new Random();
+            for(i = 0; i< n; i++)
+            {
+                for(j = 0; j<n; j++)
+                {
+                   
+                    if (ButtonsArr[i, j].Visible)
+                    {
+                        do {
+                            
+                            k = r.Next(c, (n * n)+1);
+                        }
+                        while (arr.Contains(k));
+                        arr.Add(k);
+                        ButtonsArr[i, j].Text = k.ToString();                 
+                    }
+                    else                          
+                        continue;
+                }
+            }
+
+        }
+        private void DoVisible()
+        {
+            int i, j;
+            for(i = 0; i< n; i++)
+            {
+                for(j = 0; j< n; j++)
+                {
+                    if (!ButtonsArr[i, j].Visible)
+                        ButtonsArr[i, j].Visible = true;
+                }
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            n = 4;
+            ButtonsArr = new Button[n, n];
+            int i, j;
+            int posx, posy = 30;
+
+            for (i = 0; i < 4; i++, posy += 70)
+            {
+                for (j = 0, posx = 30; j < 4; j++, posx += 130)
+                {
+                    ButtonsArr[i, j] = new Button();
+                    ButtonsArr[i, j].Name = "Button" + i.ToString() + j.ToString();
+                    ButtonsArr[i, j].Size = new Size(75, 41);
+                    ButtonsArr[i, j].Location = new Point(posx, posy);
+                    ButtonsArr[i, j].Click += ButtonClick;
+                    tabPage2.Controls.Add(ButtonsArr[i, j]);
+
+                }
+            }
+            tabControl1.SelectedIndexChanged += ChangeTab;
+            label2.Text = seconds.ToString();
+        }
+        private void ChangeTab(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 1)
+                MixNumbers();
+            if (!timer1.Enabled)
+            {
+                timer1.Start();
+            }
+           
+        }
+
+        private void Reset()
+        {
+            progressBar1.Value = 0;
+            DoVisible();
+            counter = 1;
+            textBox2.Text = "";
+        }
+        private void WriteRes()
+        {
+            button3.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true; 
+            textBox2.Text = "Attaboy !";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            timer1.Stop();
+            label1.Text = String.Format($"{seconds} sec");
+            seconds = 0;
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+
+
+            if ((sender as Button).Text == counter.ToString())
+            {
+                (sender as Button).Hide();
+                counter++;
+                MixNumbers();
+                progressBar1.Value++;
+            }
+            else
+                Reset();
+                
+            
+            if (counter == 17)
+                WriteRes();               
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+                return;
+            comboBox1.Items.Add(textBox1.Text);
+            textBox1.Text = null;
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int length = comboBox1.Items.Count;
+            if(length != 0)
+            comboBox1.Items.RemoveAt(length-1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Hide();
+            Reset();
+            label1.Visible = false;
+            timer1.Start();
+            label2.Visible = true;
+            label2.Text = seconds.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            label2.Text = seconds.ToString();
+        }
+
+ 
+    }
+}
